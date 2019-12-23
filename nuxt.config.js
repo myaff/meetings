399dc76c1d948e2db648e34c4f4f6408a9fcad59
Olga Grabko <grabka74@gmail.com>
2019-12-23 15:27:26 +0500

    upd base-path for gh-pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ const routerBase =
   process.env.DEPLOY_ENV === "GH_PAGES"
     ? {
         router: {
-          base: "/meetings/"
+          base: "/meetings-planner/"
         }
       }
     : {};
